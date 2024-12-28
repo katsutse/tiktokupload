736036196c636dc5245b0844fd1e0903ca9a4756
katsutse <katsutse@gmail.com>
2024-12-28 23:29:45 +0000

    Create reportWebVitals.js

---
+++ src/reportWebVitals.js
@@ -0,0 +1,8 @@
+// src/reportWebVitals.js
+
+const reportWebVitals = (metric) => {
+  // Log the performance metrics to the console (you can replace this with any reporting tool)
+  console.log(metric);
+};
+
+export default reportWebVitals;
